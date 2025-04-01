1c2e5cfe5143ab7fec6ae260fa694c8f13604c31
Ytalo Santos <ytalos762@gmail.com>
2025-04-01 17:34:33 -0300

    Implementação de LocalStorage para salvar dados do formulário

--- Js/validacao.js
+++ Js/validacao.js
@@ -1,7 +1,28 @@
 const formularioInsricao = document.querySelector('.formulario-informacoes');
 
+const dadosFormulario = {
+    nome: '',
+    usuario: '',
+    senha: '',
+    dataNascimento: '',
+    cpf: '',
+    sexo: '',
+    email: '',
+    telefone: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    cidade: '',
+    estado: '',
+    trilha: '',
+}
+
+const usuariosCadastrados = [];
+let idValores = 1;
+
 function validarNome() {
     const nome = document.getElementById("nome-completo-participante").value;
+    dadosFormulario.nome = nome;
     const nomeFormatoValido = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
 
     if (nome.length < 6) {
@@ -13,6 +34,7 @@ function validarNome() {
 
 function validarDataNascimento() {
     const dataNascimento = document.getElementById("data-nascimento-participante").value;
+    dadosFormulario.dataNascimento = dataNascimento;
 
     if (!dataNascimento) {
         return false;
@@ -45,6 +67,7 @@ function validarDataNascimento() {
 
 function validarCpf() {
     const cpf = document.getElementById("cpf-participante").value;
+    dadosFormulario.cpf = cpf;
     const cpfFormatoValido = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
 
     return cpfFormatoValido.test(cpf);
@@ -52,6 +75,7 @@ function validarCpf() {
 
 function validarTelefone() {
     const telefone = document.getElementById("telefone-participante").value;
+    dadosFormulario.telefone = telefone;
     const telefoneFormatoValido = /^\(\d{2}\) ?\d{4,5}-\d{4}$/;
 
     return telefoneFormatoValido.test(telefone);
@@ -59,6 +83,7 @@ function validarTelefone() {
 
 function validarEmail() {
     const email = document.getElementById("email-participante").value;
+    dadosFormulario.email = email;
     const emailFormatoValido = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     if (!email) {
@@ -70,6 +95,7 @@ function validarEmail() {
 
 function validarCep() {
     const cep = document.getElementById("cep").value;
+    dadosFormulario.cep = cep;
     const cepFormatoValido = /^\d{5}-\d{3}$/;
 
     return cepFormatoValido.test(cep);
@@ -80,6 +106,11 @@ function validarEndereco() {
     const numero = document.getElementById("numero").value;
     const cidade = document.getElementById("cidade").value;
     const estado = document.getElementById("estado").value;
+
+    dadosFormulario.rua = rua;
+    dadosFormulario.numero = numero;
+    dadosFormulario.cidade = cidade;
+    dadosFormulario.estado = estado;
 
     if (!rua || !numero || !cidade || !estado) {
         return false;
@@ -92,6 +123,7 @@ function validarTrilhas() {
 
     for (let opcaoTrilha of trilhas) {
         if (opcaoTrilha.checked) {
+            dadosFormulario.trilha = opcaoTrilha.value;
             return true;
         }
     }
@@ -110,17 +142,24 @@ function validarTermoCompromisso() {
 }
 
 function validarUsuario() {
-    const nomeUsuarioFormatoValido = /^[A-Za-z]{3,20}$/;
     const usuario = document.getElementById("usuario").value;
+    dadosFormulario.usuario = usuario;
+    const nomeUsuarioFormatoValido = /^[A-Za-z]{3,20}$/;
 
     return nomeUsuarioFormatoValido.test(usuario);
 }
 
 function validarSenha() {
     const senha = document.getElementById("senha").value;
+    dadosFormulario.senha = senha;
     const senhaFormatoValido = /^(?=.*[A-Z])(?=.*[\d@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
     return senhaFormatoValido.test(senha);
+}
+
+function salvarDados() {
+    localStorage.setItem(idValores, JSON.stringify(dadosFormulario));
+    idValores++;
 }
 
 
@@ -132,6 +171,7 @@ function exibirMensagens(erros) {
         alert(erroMsg);
         return;
     }
+    salvarDados();
     alert("Inscrição realizada com sucesso.");
 }
 
@@ -154,5 +194,7 @@ formularioInsricao.addEventListener('submit', (event) => {
     if (erros.length > 0) {
         event.preventDefault();
     }
+    const sexo = document.querySelector('#sexo-participante').value;
+    dadosFormulario.sexo = sexo;
     exibirMensagens(erros);
 });
